574376bb64b10d05124c5324999dcd12c8660bef
Helin Shiah <helinx@google.com>
2022-09-02 11:04:12 -0700

    Run command and return text

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as child_process from "child_process";
+import * as stream from "stream";
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "dartlocaldevice" is now active!');
@@ -18,6 +19,22 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from DartLocalDevice!');
 	});
+
+	// run flutter daemon
+	context.subscriptions.push(vscode.commands.registerCommand('dartlocaldevice.startDaemon', (scriptName, commandName, workingDirectory) => {
+		const process = child_process.spawn(scriptName, [commandName], { cwd: workingDirectory });
+		console.log(`process started: ${scriptName} ${commandName}`);
+		vscode.window.showInformationMessage(`process started: ${scriptName} ${commandName}`);
+
+		const promise = new Promise((resolve, reject) => {
+			process.stdout.on("data", (data: Buffer | string) => {
+				const message = data.toString();
+				resolve(message);
+			});
+			reject("Unable to start command");
+		});
+		return promise;
+	}));
 
 	context.subscriptions.push(disposable);
 }
