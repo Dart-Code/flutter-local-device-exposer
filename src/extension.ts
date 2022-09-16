7aa0eb7af2fbc2cda17a448b8daba0e8a58b75f0
Helin Shiah <helinx@google.com>
2022-09-16 14:54:25 -0700

    Add debugging for deactivate function

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,62 @@
 import * as child_process from "child_process";
+import * as fs from "fs";
 import * as vscode from 'vscode';
 
-let process: child_process.ChildProcess;
+let daemonProcess: child_process.ChildProcess;
+let logStream: fs.WriteStream;
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.commands.registerCommand('dartlocaldevice.startDaemon', async (scriptName, commandName, workingDirectory) => {
-		process = child_process.spawn(scriptName, [commandName], { cwd: workingDirectory });
-		console.log(`process started: ${scriptName} ${commandName}`);
-		vscode.window.showInformationMessage(`process started: ${scriptName} ${commandName}`);
+	context.subscriptions.push(vscode.commands.registerCommand('dartlocaldevice.startDaemon', async ({script, command, workingDirectory}) => {
+		const fileName = vscode.workspace.getConfiguration("dartlocaldevice").get<string>("logFile");
+		if (fileName) {
+			logStream = fs.createWriteStream(fileName);
+		}
+
+		daemonProcess = child_process.spawn(script, [command], { cwd: workingDirectory, shell: true, detached: true });
+		vscode.window.showInformationMessage(`process started: ${script} ${command} ${daemonProcess.pid}`);
 
 		const promise = new Promise<string>((resolve, reject) => {
-			if (process === null) {
+			if (daemonProcess === null) {
 				reject("Process was not started.");
 			}
 			// @ts-ignore: Object is possibly 'null'.
-			process.stdout.on("data", (data: Buffer | string) => {
+			daemonProcess.stdout.on("data", (data: Buffer | string) => {
 				const message = data.toString();
+				maybeLogLine(message);
 				resolve(message);
 			});
 		});
 		const result = await promise;
 		return result;
 	}));
+
+	// TODO (helin24): Remove this when deactivate is fixed.
+	context.subscriptions.push(vscode.commands.registerCommand('dartlocaldevice.killDaemon', async () => {
+		deactivate();
+	}));
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-	process.kill("SIGINT");
+export async function deactivate(): Promise<void> {
+	maybeLogLine("Deactivating");
+	maybeLogLine("Deactivating 2");
+	try {
+		maybeLogLine(`daemon process id: ${daemonProcess.pid} killed? ${daemonProcess.killed}`);
+
+		const processId = daemonProcess.pid!;
+		maybeLogLine(`found processId ${processId}`);
+	
+		const result = process.kill(-processId, "SIGINT");
+		maybeLogLine(`after process killed, result: ${result}`);
+	} catch (e) {
+		maybeLogLine(`failure: ${e}`);
+	}
+	logStream.end();
+}
+
+function maybeLogLine(content: string): void {
+	const time = new Date().toLocaleString();
+	if (logStream !== undefined) {
+		logStream.write(`${time}: ${content}\n`);
+	}
 }
